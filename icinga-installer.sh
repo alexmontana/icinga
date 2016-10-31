c040cf9452c39f194502d903c53fe856eef66233
Alex Morozov <shurik.morozov@gmail.com>
2016-10-31 14:23:12 +0200

    first commit

---
+++ icinga-installer.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+cd /usr/local/src
+rm -rf /usr/local/src/*
+
+yum -y install libdbi-devel libdbi httpd
+
+wget -O icinga.tar.gz https://sourceforge.net/projects/icinga/files/icinga/1.10.1/icinga-1.10.1.tar.gz/download
+tar -xzf icinga.tar.gz
+cd icinga-1.10.1
+
+./configure --prefix=/opt/icinga --with-icinga-user=icinga --with-icinga-group=icinga --with-httpd-conf=/etc/httpd/conf
+ make all
+ make install
+ make install-config
+ install-commandmode
+ install-webconf
+
+echo "Include conf/icinga.conf" >> /etc/httpd/conf/httpd.conf
+
+htpasswd -nb icingaadmin icingaadmin > /opt/icinga/etc/htpasswd.users
+
+/etc/init.d/httpd restart
+killall -9 icinga || /opt/icinga/bin/icinga -d /opt/icinga/etc/icinga.cfg
+
+wget https://nagios-plugins.org/download/nagios-plugins-2.1.2.tar.gz#_ga=1.2791600.1279688733.1477871534
+tar -xzf nagios-plugins-2.1.2.tar.gz 
+cd nagios-plugins-2.1.2
+./configure --prefix=/opt/icinga/ --with-nagios-user=daemon --with-nagios-group=daemon
+make
+make install
